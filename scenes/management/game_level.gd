68b75aeea127dd74dc030679a5b7e941b9200874
Gustavo Alves <119056348+GustavoALCO@users.noreply.github.com>
2023-11-24 16:34:10 -0300

    5.1

--- scenes/management/game_level.gd
+++ scenes/management/game_level.gd
@@ -10,5 +10,5 @@ func _on_cair_body_shape_entered(body_rid, body, body_shape_index, local_shape_i
 		var _reload: bool = get_tree().change_scene("res://scenes/management/dead_screen.tscn")
 
 func _on_final_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	var _reload: bool = get_tree().change_scene("res://scripts/scenes/Control.tscn")
+	var _reload: bool = get_tree().change_scene("res://scenes/management/Control.tscn")
 	
